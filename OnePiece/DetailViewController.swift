864e06dbe2dbc14a7fabec3bdb7299068c6d7a44
RYUWANWOO <reo0527@naver.com>
2021-03-17 01:02:19 +0900

    내가 처음 만든 앱 !

---
+++ OnePiece/DetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DetailViewController.swift
+//  OnePiece
+//
+//  Created by 류완우 on 2021/03/17.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func close(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
